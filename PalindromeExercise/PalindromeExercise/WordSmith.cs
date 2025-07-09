12f27cdda95a550904879cb7bc24501f9f5675be
Kaleb <Kalebreid15@gmail.com>
2025-07-08 21:46:55 -0400

    created test class for Palindrome project, passed all tests for logic.

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return false;
+            }
+
+            word = word.ToLower();
+
+            string backwards = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                backwards += word[i];
+            }
+
+            return word == backwards;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,17 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-
-        }
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Mom", true)]
+        [InlineData("Kayak", true)]
+        [InlineData("Racecar", true)]
+        public void IsAPalindrome_Test(string word, bool expected) =>
+            Assert.Equal(expected, new WordSmith().IsAPalindrome(word));
     }
 }
